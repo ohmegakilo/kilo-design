cf704887c95de2a7f13e62a47fb5e8852c6e7350
Kilo <ohmegakilo@proton.me>
2023-02-19 03:01:10 -0800

    fontchange final for tonight

--- src/components/Adventures.js
+++ src/components/Adventures.js
@@ -7,7 +7,7 @@ export default function Adventures() {
     return (
         <div className="text-white flex flex-col items-center justify-center text-center">
             <p className="pt-[110px] text-[40px] font-sans font-[300]">Current Adventures</p>
-            <p className="pr-[20px] pl-[20px] text-[rgb(86,86,86)] text-[16px] sm:text-[22px]">I am currently building a couple of projects with friends for fun</p>
+            <p className="pr-[20px] pl-[20px] font-[200] text-[rgb(86,86,86)] text-[16px] sm:text-[22px]">I am currently building a couple of projects with friends for fun</p>
             <div class="grid grid-cols-2 md:grid-cols-4 gap-7 sm:gap-9 pr-2 pl-2 sm:pl-5 sm:pr-5 mt-[30px]">
                 <a>
                     <div class="flex justify-center border-[1px] border-[rgb(34,34,34)] bg-[rgb(12,11,11)]  rounded-[30px] sm:rounded-[50px] p-6 w-[110px] h-[110px] sm:w-[150px] sm:h-[150px]"><img src={bruh} class="sm:py-3"/></div>
