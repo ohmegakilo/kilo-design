fa64593c4cc7e17370796c6e480a3bebab17490f
Kilo <ohmegakilo@proton.me>
2023-02-19 02:34:33 -0800

    aa

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,7 +13,7 @@ export default function Hero() {
     <div class="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
         <video class="min-w-full min-h-full absolute object-cover" src={vid} type="video/mp4" autoPlay={true} muted loop></video>
     </div>
-    <div class=" z-10 grid place-items-center h-[800px] w-[100%] text-center">
+    <div class=" z-10 grid place-items-center h-[800px]  w-[100%] text-center">
             <iframe class="h-[440px] w-full mt-[50px]" src='https://my.spline.design/untitled-ec69b075a9f0ecd74c57339b8b6a315f/' frameborder='0'></iframe>
             <h1 class=" font-serif font-[600] text-[35px] sm:text-[45px] md:text-[55px] text-[rgb(223,223,223)]">Designer Of Sorts</h1>
             <h1 class="px-[35px] font-mono text-[12px] sm:text-[14px] md:text-[16px] text-[rgb(68,68,68)] ">Graphic Designer - UI/UX Design - 3D Artist - Frontend & Javascript development - Creative Content</h1>
