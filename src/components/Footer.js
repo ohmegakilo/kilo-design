c10f5da612b5e60266f2fca72637e79b689382e3
Kilo <ohmegakilo@proton.me>
2023-02-19 17:18:07 -0800

    fix foot

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import instagram from './img/instagram.png'
 import kilo from './img/kilo.png'
 import twitter from './img/twitter.png'
 import behance from './img/behance.png'
+import yt from './img/yt.png'
+import git from './img/git.png'
 
 
 export default function Footer() {
@@ -12,17 +14,25 @@ export default function Footer() {
                         class="w-[55px] sm:w-[65px]"/></a>
             </div>
 
-            <div class=" space-x-[13px] ml-3">
-                <button class="py-3 px-3 rounded-[30px] text-sm text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)]">
+            <div class=" space-x-[10px] ml-3">
+                <button class="py-2 px-2 sm:py-3 sm:px-3 rounded-[30px] text-sm text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)]">
+                    <img src={twitter}
+                        class=' w-[13px]'/>
+                </button>
+                <button class="py-2 px-2 sm:py-3 sm:px-3 rounded-[30px] text-sm text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)]">
                     <img src={instagram}
                         class=' w-[13px]'/>
                 </button>
-                <button class="py-3 px-3 rounded-[30px] text-sm text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)]">
+                <button class="py-2 px-2 sm:py-3 sm:px-3 rounded-[30px]  text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)] ">
                     <img src={behance}
-                        class=' w-[13px]'/>
+                        class=' w-[15px]'/>
                 </button>
-                <button class="py-3 px-3 rounded-[30px]  text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)] ">
-                    <img src={twitter}
+                <button class="py-2 px-2 sm:py-3 sm:px-3 rounded-[30px]  text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)] ">
+                    <img src={yt}
+                        class=' w-[15px]'/>
+                </button>
+                <button class="py-2 px-2 sm:py-3 sm:px-3 rounded-[30px]  text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)] ">
+                    <img src={git}
                         class=' w-[15px]'/>
                 </button>
             </div>
