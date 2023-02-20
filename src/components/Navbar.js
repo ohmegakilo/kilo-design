4e17675e548080bfb26d87626a15c9d6986daf1d
Kilo <ohmegakilo@proton.me>
2023-02-19 17:54:03 -0800

    fix buutton smh why this pain

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,18 +19,18 @@ export default function Navbar() {
             </div>
 
             <div class="sm:hidden space-x-[13px] ml-3">
-                <button href="#"  class="py-3 px-3 rounded-[30px] text-sm text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)]">
+                <a  href="#"  class="py-3 px-3 rounded-[30px] text-sm text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)]">
                     <img src={home}
                         class=' w-[15px]'/>
-                </button>
-                <button  href="https://www.behance.net/ohmegakilo" class="py-3 px-3 rounded-[30px] text-sm text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)]">
+                </a>
+                <a  href="https://www.behance.net/ohmegakilo" class="py-3 px-3 rounded-[30px] text-sm text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)]">
                     <img src={portfolio}
                         class=' w-[15px]'/>
-                </button>
-                <button  href="mailto:ohmegakilo@proton.me" class="py-3 px-3 rounded-[30px]  text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)] ">
+                </a>
+                <a  href="mailto:ohmegakilo@proton.me" class="py-3 px-3 rounded-[30px]  text-center font-[600] border-[1px] inline-flex items-center border-[rgb(34,34,34)] bg-gradient-to-t from-[rgb(16,14,25)] to-[rgb(34,32,47)] ">
                     <img href="mailto:ohmegakilo@proton.me" src={message}
                         class=' w-[15px]'/>
-                </button>
+                </a>
             </div>
 
             <button class="py-3 px-4 sm:px-5 rounded-[30px] hover:bg-[rgb(14,46,9)] text-[13px]  text-center text-gray-100 hover:text-white font-[600] border-[1px] border-gray-600 hover:border-green-300 hover:shadow-md transform active:scale-75 transition-transform focus:border-1.4 focus:border-green-300 duration-700 focus:outline-none inline-flex items-center ">
