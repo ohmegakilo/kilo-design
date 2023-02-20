97b9bd2780b0c3d446c352165683850a9f34d19c
Kilo <ohmegakilo@proton.me>
2023-02-19 19:20:14 -0800

    ayeee

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,12 +11,13 @@ export default function Hero() {
         
         
 
-        <section class="relative h-[720px] sm:h-[750px] flex flex-col items-center justify-center text-center text-white py-0 px-3">
+        <section class="relative h-[620px] sm:h-[750px] flex flex-col items-center justify-center text-center text-white py-0 px-3">
     <div class="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
         <video class="min-w-full min-h-full absolute object-cover" src={vid} type="video/mp4" autoPlay={true} muted loop></video>
     </div>
     <div class=" z-10 grid place-items-center h-[800px]  w-[100%] text-center">
-            <iframe class="h-[440px] w-full mt-[50px]" src='https://my.spline.design/untitled-ec69b075a9f0ecd74c57339b8b6a315f/' frameborder='0'></iframe>
+            <iframe  class=" hidden sm:block h-[440px] w-full mt-[50px] " src='https://my.spline.design/untitled-ec69b075a9f0ecd74c57339b8b6a315f/' ></iframe>
+            <iframe  class="sm:hidden h-[440px] w-full mt-[50px] " src='https://my.spline.design/mobile-ff17b726a5e0b0f2d277817ce9b9de8f/' ></iframe>
             <h1 class=" mt-[5px] font-serif font-[600] text-[35px] sm:text-[45px] md:text-[55px] text-[rgb(223,223,223)]">Designer Of Sorts</h1>
             <h1 class="mt-[12px] mb-[10px] px-[35px] font-mono text-[12px] sm:text-[14px] md:text-[16px] text-[rgb(68,68,68)] ">Graphic Designer - UI/UX Design - 3D Artist - Frontend & Javascript development - Creative Content</h1>
             <span class=" flex space-x-[17px] mb-[190px] mt-3">
